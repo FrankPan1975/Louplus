31264c0dd071745fca23375b49c69c58f26c2bee
Pan <flagpan@189.cn>
2020-02-21 23:52:30 +0800

    jump7

---
+++ jump7.py
@@ -0,0 +1,13 @@
+i = 1
+
+while i <= 100 :
+    #'''
+    #print("i%10=",i%10)
+    if i%7 == 0 or i % 10 == 7 or i // 10 ==7:
+        i=i+1
+        continue
+    
+    #'''
+    print(i)
+    i = i+1
+
